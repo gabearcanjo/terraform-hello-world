723daabf10b913771256e58d9b3f86e0811deb8e
Kledson Basso <kledsonhugo@gmail.com>
2023-08-02 21:46:29 -0300

    Pipeline setup

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,20 +1,20 @@
 # PROVIDER
 terraform {
 
-  required_version = "~> 1.4"
+  required_version = "~> 1.5"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.55"
+      version = "~> 5.10"
     }
   }
 
-}
+  backend "s3" {
+    bucket         = "tf-terraform-hello-world-state-v1"
+    key            = "terraform.tfstate"
+    dynamodb_table = "tf-terraform-hello-world-state-v1"
+    region         = "us-east-1"
+  }
 
-provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = [".aws/config"]
-  shared_credentials_files = [".aws/credentials"]
-  profile                  = "fiap-iac"
 }
